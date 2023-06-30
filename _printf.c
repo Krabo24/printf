0c743a5b3bbfbc2c57c0eebb7e1c6046c3e01079
Krabs <krabo24@yahoo.com>
2023-06-30 16:00:19 -0700

    functions2.c

--- _printf.c
+++ _printf.c
@@ -3,13 +3,11 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - printf function
- * @format: format
- * Return: printed chars
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
-
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -55,13 +53,11 @@ int _printf(const char *format, ...)
 }
 
 /**
- * print_buffer - print contents of buffer if it exist
- * @buffer: array of chars
- * @buff_ind: index to add next char, display length
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-
 void print_buffer(char buffer[], int *buff_ind)
-
 {
 	if (*buff_ind > 0)
 		write(1, &buffer[0], *buff_ind);
